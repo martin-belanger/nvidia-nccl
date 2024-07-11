790be9f564fca3a9135cb65d3f7c80be5dac0549
Martin Belanger <martin.belanger@dell.com>
2024-07-11 16:39:46 -0400

    Saved NCCL stats to shared memory as a versioned C struct (version=1)
    Signed-off-by: Martin Belanger <martin.belanger@dell.com>

--- src/dell/collector.cc
+++ src/dell/collector.cc
@@ -22,34 +22,6 @@ PRE_AND_POST_ENUM_INCREMENT(ncclFunc_t)
 PRE_AND_POST_ENUM_INCREMENT(ncclRedOp_t)
 PRE_AND_POST_ENUM_INCREMENT(ncclDataType_t)
 
-
-/**
- * Calculates the smallest integral power of two that is not
- * smaller than @value.
- */
-static unsigned long long ceil2(size_t value) {
-	return 1ULL << (log2i(value) + 1);
-}
-
-#if (WITH_BYTES)
-static size_t data_size(ncclDataType_t data_type) {
-	switch (data_type) {
-	case ncclInt8:     return 1;
-	case ncclUint8:    return 1;
-	case ncclInt32:    return 4;
-	case ncclUint32:   return 4;
-	case ncclInt64:    return 8;
-	case ncclUint64:   return 8;
-	case ncclFloat16:  return 2;
-	case ncclFloat32:  return 4;
-	case ncclFloat64:  return 8;
-	case ncclBfloat16: return 2;
-	default:;
-	}
-	return 0;
-}
-#endif // WITH_BYTES
-
 static const char* func_name(ncclFunc_t func) {
 	switch (func) {
 	case ncclFuncBroadcast:     return "Broadcast";
@@ -213,119 +185,15 @@ static std::string get_shmpath(int rank)
 	return "/prometheus-nccl-" + std::to_string(rank);
 }
 
-/**
- * Stringify a nccl_stats_c object. The output will be a string
- * composed of comma-separated '"key":value' pairs as in this
- * example:
- *
- * "opers":value,"count":value,"count-[1K,2K)":value,"count-[2K,4K)":value,"chunks":value
- */
-std::string nccl_stats_c::to_string() const
+std::ostream& operator<<(std::ostream  & stream_r, const nccl_data_v1_c  * data_p)
 {
-	std::string s;
-
-	s += "\"opers\":";
-	s += std::to_string(opers);
-
-	s += ",\"count\":";
-	s += std::to_string(count);
-
-	s +=  ",\"chunks\":";
-	s += std::to_string(chunks);
+	stream_r << "  opers: \t" << data_p->opers  << '\n'
+		 << "  count: \t" << data_p->count  << '\n'
+		 << "  chunks:\t" << data_p->chunks << '\n';
 
 	for (auto id = 0; id < MAX_NUM_BINS; id++) {
-		if (pw2_counts[id]) {
-			s += ",\"count-";
-			s += pwr2_interval(id);
-			s += "\":";
-			s += std::to_string(pw2_counts[id]);
-		}
-	}
-
-#if (WITH_BYTES)
-	s +=  ",\"bytes\":";
-	s += std::to_string(bytes);
-#endif // WITH_BYTES
-
-	return s;
-}
-
-/**
- * Stringify a kv_obj_c object. The output will be a string
- * composed of comma-separated '"key":{value}' pairs as in this
- * example:
- *
- * "key1":{value1},"key2":{value2},"key3":{value3},...
- *
- */
-void kv_obj_c::append(const char * key, const std::string & val)
-{
-	val_m += sep_m;
-	val_m += '"';
-	val_m += key;
-	val_m += "\":{";
-	val_m += val;
-	val_m += '}';
-	sep_m = ",";
-}
-
-/**
- * Convert the struct "stats_m" to a string of "key:value"
- * pairs.
- */
-std::string dell_collector_c::stringify()
-{
-	kv_obj_c  func, oper, type, algo, prot;
-
-	for (auto ifunc = (ncclFunc_t)0; ifunc < ncclNumFuncs; ifunc++) {
-		oper.clear();
-
-		for (auto ioper = (ncclRedOp_t)0; ioper < ncclNumOps; ioper++) {
-			type.clear();
-
-			for (auto itype = (ncclDataType_t)0; itype < ncclNumTypes; itype++) {
-				algo.clear();
-
-				for (auto ialgo = 0; ialgo < NCCL_NUM_ALGORITHMS; ialgo++) {
-					prot.clear();
-
-					for (auto iprot = 0; iprot < NCCL_NUM_PROTOCOLS; iprot++) {
-						auto stats_p = &stats_m[ifunc][ioper][itype][ialgo][iprot];
-						if (!stats_p->empty())
-							prot.append(proto_name(iprot), stats_p->to_string());
-					}
-
-					if (!prot.empty())
-						algo.append(algo_name(ialgo), prot);
-				}
-
-				if (!algo.empty())
-					type.append(type_name(itype), algo);
-			}
-
-			if (!type.empty())
-				oper.append(op_name(ioper), type);
-		}
-
-		if (!oper.empty())
-			func.append(func_name(ifunc), oper);
-	}
-
-	return '{' + func.str() + "}\n";
-}
-
-std::ostream& operator<<(std::ostream  & stream_r, const nccl_stats_c  * stats_p)
-{
-	stream_r << "  opers: \t" << stats_p->opers  << '\n'
-		 << "  count: \t" << stats_p->count  << '\n'
-#if (WITH_BYTES)
-		 << "  bytes: \t" << stats_p->bytes  << '\n'
-#endif // WITH_BYTES
-		 << "  chunks:\t" << stats_p->chunks << '\n';
-
-	for (auto id = 0; id < MAX_NUM_BINS; id++) {
-		if (stats_p->pw2_counts[id])
-			stream_r << "  bins[" << id << "]:\t" << stats_p->pw2_counts[id] << '\t' << pwr2_interval(id) << '\n';
+		if (data_p->pw2_counts[id])
+			stream_r << "  bins[" << id << "]:\t" << data_p->pw2_counts[id] << '\t' << pwr2_interval(id) << '\n';
 	}
 
 	return stream_r;
@@ -345,15 +213,15 @@ void dell_collector_c::print_counters()
 			for (auto itype = (ncclDataType_t)0; itype < ncclNumTypes; itype++) {
 				for (auto ialgo = 0; ialgo < NCCL_NUM_ALGORITHMS; ialgo++) {
 					for (auto iprot = 0; iprot < NCCL_NUM_PROTOCOLS; iprot++) {
-						auto stats_p = &stats_m[ifunc][ioper][itype][ialgo][iprot];
-						if (!stats_p->empty()) {
+						auto data_p = &shmp_m->data_m[ifunc][ioper][itype][ialgo][iprot];
+						if (!data_p->empty()) {
 							std::cout << "stats["
 								  << func_name(ifunc)  << "]["
 								  << op_name(ioper)    << "]["
 								  << type_name(itype)  << "]["
 								  << algo_name(ialgo)  << "]["
 								  << proto_name(iprot) << "]:\n"
-								  << stats_p
+								  << data_p
 								  << '\n';
 						}
 					}
@@ -364,93 +232,46 @@ void dell_collector_c::print_counters()
 	std::cout << '\n';
 }
 
-bool dell_collector_c::shm_resize(size_t length)
-{
-	// New size is rounded up to next power of 2.
-	auto new_size = ceil2(length);
-
-	if (new_size > SHM_MAX_SIZE) {
-		WARN("Prometheus: Shared memory required is too big sz=%ld (max: %dM)",
-		     length, SHM_MAX_SIZE/(1024*1024));
-		shm_max_reached_m = true;
-		return false;
-	}
-
-	if (-1 == ftruncate(fd_m, new_size)) {
-		WARN("Prometheus: failed to resize shared memory sz=%lld", new_size);
-		return false;
-	}
-
-	shmp_m = mremap(shmp_m, shms_m, new_size, MREMAP_MAYMOVE, NULL);
-	if (shmp_m == MAP_FAILED) {
-		WARN("Prometheus: Failed to mremap shared memory sz=%lld", new_size);
-		return false;
-	}
-
-	shms_m = new_size;
-
-	return true;
-}
-
-void dell_collector_c::shm_save()
-{
-	if (shmp_m && !shm_max_reached_m) {
-		// Write JSON to shared memory
-		auto text = stringify();
-
-		// Make sure the SHM is big enough
-		if (text.length() > shms_m) {
-			if (!shm_resize(text.length()))
-				return;
-		}
-
-		memcpy(shmp_m, text.c_str(), text.length());
-	}
-}
-
-void dell_collector_c::shm_unlink()
-{
-	std::string shmpath = get_shmpath(rank_m);
-	::shm_unlink(shmpath.c_str());
-}
-
 dell_collector_c::dell_collector_c(int rank) : rank_m(rank)
 {
-	shm_unlink(); // Ensure there is no leftover SHM from a previous run.
+	std::string shmpath = get_shmpath(rank_m);
+	shm_unlink(shmpath.c_str()); // Ensure there is no leftover SHM from a previous run.
 
-	fd_m = shm_open(get_shmpath(rank_m).c_str(), O_CREAT | O_EXCL | O_RDWR, 0644);
+	fd_m = shm_open(shmpath.c_str(), O_CREAT | O_EXCL | O_RDWR, 0644);
 	if (fd_m == -1) {
 		WARN("Prometheus: Failed to create shared memory");
 		return;
 	}
 
-	if (-1 == ftruncate(fd_m, shms_m)) {
+	if (ftruncate(fd_m, shms_m) == -1) {
 		WARN("Prometheus: Failed to resize shared memory");
 		goto error;
 	}
 
 	/* Map the shm object into the caller's address space. */
-	shmp_m = mmap(NULL, shms_m, PROT_READ | PROT_WRITE, MAP_SHARED, fd_m, 0);
-	if (shmp_m == MAP_FAILED) {
+	shmp_m = (nccl_stats_c *)mmap(NULL, shms_m, PROT_READ | PROT_WRITE, MAP_SHARED, fd_m, 0);
+	if ((void *)shmp_m == MAP_FAILED) {
 		WARN("Prometheus: Failed to mmap shared memory");
 		goto error;
 	}
+
+	shmp_m->clear();
 
 	return;
 
 error:
 	close(fd_m);
-	shm_unlink();
+	shm_unlink(shmpath.c_str());
 
 	fd_m = -1;
-	shmp_m = MAP_FAILED;
+	shmp_m = (nccl_stats_c *)MAP_FAILED;
 }
 
 dell_collector_c::~dell_collector_c(void)
 {
 	if (shmp_m) {
 		munmap(shmp_m, 0);
-		shmp_m = NULL;
+		shmp_m = nullptr;
 	}
 
 	if (fd_m != -1) {
@@ -458,7 +279,7 @@ dell_collector_c::~dell_collector_c(void)
 		fd_m = -1;
 	}
 
-	shm_unlink();
+	shm_unlink(get_shmpath(rank_m).c_str());
 }
 
 void dell_collector_c::collect(const struct ncclInfo *info_p)
@@ -469,17 +290,12 @@ void dell_collector_c::collect(const struct ncclInfo *info_p)
 	    (info_p->algorithm < NCCL_NUM_ALGORITHMS) &&
 	    (info_p->protocol  < NCCL_NUM_PROTOCOLS)
 	   ) {
-		auto stats_p = &stats_m[info_p->coll][info_p->op][info_p->datatype][info_p->algorithm][info_p->protocol];
+		auto data_p = &shmp_m->data_m[info_p->coll][info_p->op][info_p->datatype][info_p->algorithm][info_p->protocol];
 
-		stats_p->opers++;
-		stats_p->count += info_p->count;
-		stats_p->chunks += info_p->chunkCount;
-		stats_p->pw2_counts[pwr2_index(info_p->count)]++;
-#if (WITH_BYTES)
-		stats_p->bytes += info_p->count * info_p->chunkCount * data_size(info_p->datatype);
-#endif // WITH_BYTES
-
-		shm_save();
+		data_p->opers++;
+		data_p->count += info_p->count;
+		data_p->chunks += info_p->chunkCount;
+		data_p->pw2_counts[pwr2_index(info_p->count)]++;
 	}
 }
 
--- src/include/dell/collector.h
+++ src/include/dell/collector.h
@@ -11,49 +11,31 @@
 
 #ifdef DELL_COLLECTOR
 
-#include <string.h>  // memset()
+#define MAX_NUM_BINS  32
+#define SHM_VERSION   1
 
-#define SHM_INITIAL_SIZE (64 * 1024)
-#define SHM_MAX_SIZE     (4 * 1024 * 1024)
-
-#define WITH_BYTES       0
-#define MAX_NUM_BINS     32
-
-class nccl_stats_c {
-public:
+struct nccl_data_v1_c {
 	bool empty() const { return opers == 0; }
-	std::string to_string() const;
 
 	unsigned long long opers;
 	unsigned long long count;
 	unsigned long long chunks;
 	unsigned long long pw2_counts[MAX_NUM_BINS];
-
-#if (WITH_BYTES)
-	unsigned long long bytes;
-#endif // WITH_BYTES
-
 };
 
-std::ostream& operator<<(std::ostream  & stream_r, const nccl_stats_c  * stats_p);
-
-class kv_obj_c
-{
-public:
+struct nccl_stats_c {
 	void clear() {
-		val_m.clear();
-		sep_m.clear();
+		memset((void *)this, 0, sizeof(*this));
+		version_m = SHM_VERSION;
 	}
-	bool empty() const { return val_m.empty(); }
-	void append(const char * key, const std::string & val_r);
-	void append(const char * key, const kv_obj_c & val_r) {append(key, val_r.val_m);}
 
-	const std::string & str() const { return val_m; }
-
-protected:
-	std::string   val_m;
-	std::string   sep_m;
+	unsigned long long  version_m;
+	nccl_data_v1_c      data_m[ncclNumFuncs][ncclNumOps][ncclNumTypes][NCCL_NUM_ALGORITHMS][NCCL_NUM_PROTOCOLS];
 };
+
+#define SHM_SIZE  sizeof(nccl_stats_c)
+
+std::ostream& operator<<(std::ostream  & stream_r, const nccl_data_v1_c  * stat_p);
 
 class dell_collector_c {
 public:
@@ -61,22 +43,16 @@ public:
 	~dell_collector_c(void);
 
 	void collect(const struct ncclInfo *collInfo);
-	std::string stringify();
 	void print_counters();
 
-	void shm_save();
-	void shm_unlink();
 	const char *shm_data() { return (const char *)shmp_m; }
 	size_t shm_size() { return shms_m; }
-	bool shm_resize(size_t length);
 
 protected:
 	int           rank_m  = -1;
-	int           fd_m    = -1;               // Shared memory file descriptor
-	size_t        shms_m  = SHM_INITIAL_SIZE; // Shared memory size
-	void         *shmp_m  = MAP_FAILED;       // Pointer to shared memory (mmap)
-	nccl_stats_c  stats_m[ncclNumFuncs][ncclNumOps][ncclNumTypes][NCCL_NUM_ALGORITHMS][NCCL_NUM_PROTOCOLS] = {0};
-	bool          shm_max_reached_m = false;
+	int           fd_m    = -1;       // Shared memory file descriptor
+	size_t        shms_m  = SHM_SIZE; // Shared memory size
+	nccl_stats_c *shmp_m  = (nccl_stats_c *)MAP_FAILED; // Pointer to shared memory (mmap)
 };
 
 /**
